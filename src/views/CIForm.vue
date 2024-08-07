e3ba0513799a958aac3137fa5b641ede67c38d40
kei <kein312@gmail.com>
2024-08-07 10:11:07 +0900

    CIFormにメソッド呼び出しのコード追加。ref, defineExpose, getSummaryText...()

--- src/views/CIForm.vue
+++ src/views/CIForm.vue
@@ -1,5 +1,6 @@
 <template>
     <StrokeForm 
+    ref="strokeChild"
     :strokeTypeSelection="strokeTypeSelection" 
     :aspectShow="true" 
     :lesionSizeSelection="lesionSizeSelection"
@@ -7,14 +8,25 @@
     :showtPATreatment="true"
     :showEndovascularTreatment="true"
     />
-  </template>
+</template>
   
-  <script setup>
+<script setup>
   import StrokeForm from './StrokeForm.vue';
   const strokeTypeSelection = ['アテローム血栓性', '心原性塞栓', 'ラクナ', 'その他'];
   const lesionSizeSelection = ['15mm未満（小）', '15mm以上1/2葉未満（中）', '1/2葉以上（大）', '不明'];
-  </script>
 
-  <style scoped>
+  // 親コンポーネントから子コンポーネントのメソッドを呼び出す
+  const mvdChild = ref(null);
+
+  const getSummaryTextFromGrandChild = () => {
+    return strokeChild.value.getSummaryOfStroke();
+  }
+
+  defineExpose({
+    getSummaryTextFromGrandChild,
+  });
+</script>
+
+<style scoped>
   /* Add your styles here */
-  </style>
+</style>

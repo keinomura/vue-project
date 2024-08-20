7c4fb4957855f26464b29607df06ba66257d363a
kei <kein312@gmail.com>
2024-08-20 10:15:06 +0900

    退院サマリ完成

--- src/views/SummaryAtDischargeViews/OthersAtDViews/OthersAtDForm.vue
+++ src/views/SummaryAtDischargeViews/OthersAtDViews/OthersAtDForm.vue
@@ -1,36 +1,44 @@
 <template>
-  <v-container style="background-color: lightblue">
-    <v-row>
-      <v-radio-group v-model="admissionPurpose" label="今回入院目的" inline>
-        <v-radio label="精査" value="精査"></v-radio>
-        <v-radio label="治療" value="治療"></v-radio>
+  <v-container style="background-color: yellowgreen">
+    <!-- CSDH -->{{ diseaseName }}
+    <v-container v-if="diseaseName === 'CSDH'">
+      <v-radio-group v-model="operation" label="手術" inline>
+        <v-radio label="あり" value="あり"></v-radio>
+        <v-radio label="なし" value="なし"></v-radio>
+      </v-radio-group>
+      <v-container v-if="operation === 'あり'">
+        <v-radio-group v-model="operationDate" label="手術日選択" inline>
+          <v-radio label="入院同日" value="入院同日"></v-radio>
+          <v-radio label="入院翌日" value="入院翌日"></v-radio>
+          <v-radio label="その他" value="その他"></v-radio>
+        </v-radio-group>
+        <v-text-field v-if="operationDate === 'その他'" v-model="operationDateText" label="入院後手術までの日数"></v-text-field>
+        <v-radio-group v-model="operationMethod" label="手術方法" inline>
+          <v-radio label="穿頭洗浄術" value="穿頭洗浄術"></v-radio>
+          <v-radio label="穿頭ドレナージ" value="穿頭ドレナージ"></v-radio>
+          <v-radio label="その他" value="その他"></v-radio>
+        </v-radio-group>
+        <v-text-field v-if="operationMethod === 'その他'" v-model="operationMethodText" label="手術方法"></v-text-field>
+      </v-container>
+      <v-container v-if="operation === 'なし'">
+        <v-text-field v-model="observationPeriod" label="経過観察日数"></v-text-field>
+      </v-container>
+    </v-container>
+
+    <!-- 経過観察 -->
+    <v-container v-if="diseaseName === 'observation'">
+      <v-radio-group v-model="followUp" label="退院後フォロー" inline>
+        <v-radio label="当院フォロー" value="当院フォロー"></v-radio>
+        <v-radio label="他院フォロー" value="他院フォロー"></v-radio>
+        <v-radio label="フォロー不要" value="フォロー不要"></v-radio>
         <v-radio label="その他" value="その他"></v-radio>
       </v-radio-group>
-    </v-row>
+      <v-text-field v-if="followUp === 'その他'" v-model="followUpText" label="退院後フォロー"></v-text-field>
+    </v-container>
 
-<!-- 精査 -->{{ admissionPurpose }}
-  <v-container v-if="admissionPurpose === '精査'">
-    <v-textarea v-model="examinationFindings" label="検査所見"></v-textarea>
-
-    <v-radio-group v-model="treatmentPlan" label="今後の治療方針" inline>
-      <v-radio label="手術適応あり" value="手術適応あり"></v-radio>
-
-      <v-radio label="経過観察 定期フォローアップ" value="経過観察 定期フォローアップ"></v-radio>
-      <v-radio label="他院フォローアップ" value="他院フォローアップ"></v-radio>
-      <v-radio label="要コンサルト" value="要コンサルト"></v-radio>
-      <v-radio label="その他" value="その他"></v-radio>
-    </v-radio-group>
-    <v-row v-if="treatmentPlan === '手術適応あり'">
-      <v-checkbox v-model="surgicalIndications" label="手術適応" value="手術適応"></v-checkbox>
-      <v-checkbox v-model="surgicalIndications" label="血管内治療適応" value="血管内治療適応"></v-checkbox>
-    </v-row>
-    <v-text-field v-if="treatmentPlan === 'その他'" v-model="treatmentPlanText" label="その他"></v-text-field>
-  </v-container>
-
-<!-- 治療 -->
-  <v-container v-if="admissionPurpose === '治療'">
-    <v-textarea v-model="treatmentFindings" label="治療所見" ></v-textarea>
-  </v-container>
+    <!-- サマリ -->
+    <v-textarea v-model="othersSummary" label="サマリ"></v-textarea>
+    
   </v-container>
 
 
@@ -46,64 +54,50 @@
   
 
   // Data
-  const admissionPurpose = ref('精査');
-  const examinationFindings = ref('');
-  const treatmentFindings =  ref('');
+  const operation = ref('あり');
+  const operationDate = ref('入院同日');
+  const operationDateText = ref('');
+  const operationMethod = ref('穿頭洗浄術');
+  const operationMethodText = ref('');
+  const observationPeriod = ref('');
+  const followUp = ref('当院フォロー');
+  const followUpText = ref('');
+  const othersSummary = ref('');
 
-  watch(admissionPurpose, (newVal) => {
-    if (newVal === '治療') {
-      treatmentFindings.value = '予定通り、手術施行。経過良好にて退院となった。';
-      examinationFindings.value = '';
-    } else if (newVal === '精査') {
-      treatmentFindings.value = '';
-    }
-  }, {immediate: true});
-
-  const treatmentPlan = ref('手術適応あり');
-  const surgicalIndications = ref([]);
-  const treatmentPlanText = ref('');
-
-
-
-  
-  const additionalNotes = ref('');
-  
 
   // Methods
+  // CSDHのサマリを作成
+  const createCSDHSummary = () => {
+    // 手術日のText
+    const operationDateString = (operationDate.value === 'その他') ? '入院後' + operationDateText.value + '日' : operationDate.value;
+    const operationMethodString = (operationMethod.value === 'その他') ? operationMethodText.value : operationMethod.value;
+
+    othersSummary.value = (operation.value === 'なし') ? 
+    // 手術無し
+                          '症状悪化なく経過観察期間: ' + observationPeriod.value + '日にて退院。':
+    // 手術有り
+                          '手術日: ' + operationDateString + '、手術方法: ' + operationMethodString + 'にて手術を施行。経過観良好にて退院。';
+  };
+
+  const createObservationSummary = () => {
+    const followUpString = (followUp.value === 'その他') ? followUpText.value : followUp.value;
+
+    othersSummary.value = '経過観察し、症状悪化無く退院した。退院後フォロー: ' + followUpString + '。';
+  };
+
+
+  watch([operation, operationDate, operationDateText, operationMethod, operationMethodText, observationPeriod, props, followUp, followUpText],() => {
+    (props.diseaseName === 'CSDH') ? createCSDHSummary():(props.diseaseName === 'observation') ? createObservationSummary():'';
+  }, {immediate: true});
   
-  
-  const getSummaryAtDischargeOfAsymptom = () => {
-    
 
-    const AsymptomaticSummary = {
-      '今回入院目的': admissionPurpose.value,
-      'その他': treatmentPlanText.value,
-
-      '検査所見': examinationFindings.value,
-      '治療所見': treatmentFindings.value,
-      '今後の治療方針': treatmentPlan.value,
-      '手術適応': surgicalIndications.value,
-      'その他備考': additionalNotes.value,
-      
-    };
-
-    
-
-    // 項目が空のものを除外
-    const filteredAsymptomaticSummary = Object.fromEntries(
-      Object.entries(AsymptomaticSummary).filter(([key, value]) => value != null && value !== '' && value !== undefined && value.length !== 0)
-    );
-
-    const summaryText = Object.entries(filteredAsymptomaticSummary)
-    .map(([key, value]) => `${key}: ${value}`)
-    .join(',');
-
-    return summaryText; 
+  const getSummaryAtDischargeOfOthers = () => {
+    return othersSummary.value;
   };
 
   // "defineExpose"を使用して、外部から参照できるプロパティを定義する
   defineExpose({
-    getSummaryAtDischargeOfAsymptom,
+    getSummaryAtDischargeOfOthers,
   });
 
   // Watchers

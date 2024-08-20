99d8be23400a4d169810dee80e07ae94a6032b84
kei <kein312@gmail.com>
2024-08-20 14:45:02 +0900

    不要なファイルを削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SummaryForm from '../views/SummaryViews/SummaryForm.vue';
 import SummaryAtDischargeForm from '../views/SummaryAtDischargeViews/SummaryAtDischargeForm.vue';
-import SurgeryForm from '../views/SurgeryForm.vue';
+// import SurgeryForm from '../views/SurgeryForm.vue';
 import SummaryFrame from '../views/SummaryFrame.vue';
 
 const routes = [
   { path: '/summary', component: SummaryForm },
   { path: '/summaryAtDischarge', component: SummaryAtDischargeForm },
-  { path: '/surgery', component: SurgeryForm },
+  // { path: '/surgery', component: SurgeryForm },
   { path: '/summaryFrame', component: SummaryFrame, props: {title: '入院サマリですよ'}},
   // { path: '/summaryFrame', component: SummaryFrame},
 

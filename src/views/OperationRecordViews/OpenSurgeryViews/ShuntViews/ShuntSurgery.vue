32d31f6da3c628b758d47788e651bba6fb547982
kei <kein312@gmail.com>
2024-08-30 14:05:46 +0900

    サマリの作成改善

--- src/views/OperationRecordViews/OpenSurgeryViews/ShuntViews/ShuntSurgery.vue
+++ src/views/OperationRecordViews/OpenSurgeryViews/ShuntViews/ShuntSurgery.vue
@@ -155,37 +155,45 @@
     //operation texts
     const operationInfo = '頭部' + (operationType.value === 'V-P shunt') ? operationSide.value + 'VPシャント術':'LPシャント術';
 
-    const TextOfBurrHoleAndPocketForBulb =
-        (punctureSite.value === '前角') ? '正中から'+ operationSide.value + 'に3cm、Coronal sutureの前方1cmにBurr holeを開けるべく、皮膚切開を行った。'
+    // realtimeに変わらなくてもいい。最終的な記録を作成するときに使う　-> functionにする
+    const TextOfBurrHoleAndPocketForBulb = () => {
+      return (punctureSite.value === '前角') ? '正中から'+ operationSide.value + 'に3cm、Coronal sutureの前方1cmにBurr holeを開けるべく、皮膚切開を行った。'
         :'Inionの上方6cm、正中から'+ operationSide.value + '3cmの位置にBurr holeを開けるべく、皮膚切開を行った。'
         + '皮膚切開は' + skinIncision.value + 'に行った。' + '筋層を切開剥離し、骨を露出した。Perforatorを用いてburr holeを開けた。'
         + '骨を止血後、硬膜をバイポーラで止血凝固。尖刃で硬膜を十字切開し脳表を確認。脳表をバイポーラで凝固し尖刃で切開した。'
         + 'あらかじめドレナージチューブを表皮から皮下に挿入しておいた。'
-      const TextOfLumbarPunctureAndPocketForBulb = '穿刺レベル' + punctureLevel.value + 'の傍正中にlinearに皮膚切開を行った。'
-      + '穿刺針を' + punctureLevel.value + 'に向けて'+ punctureSite.value + 'から挿入した。'
-      + '穿刺針よりCSFの流出を確認した。穿刺針より脊髄腔内に近位ドレナージチューブを'+ punctureLength.value +'cm挿入した。'
-      + '皮下にシャント圧調整バルブを挿入するスペースを作った。'
-
-      const TextOfVentPuncture = (punctureSite.value === '前角') ? '穿刺針をnasion, 外耳孔に向けて挿入した。':'穿刺針をnasionに向けて挿入した。'
-      +'穿刺針よりCSFの流出を確認した。穿刺針を抜き、同じTractに近位ドレナージチューブを挿入した。'
-      + measurementPoint.value + 'を基準に近位ドレナージチューブを'+ punctureLength.value +'cm挿入して固定した。'
-
-      const TextOfAbdominalSide = '臍'+ abdominalSide.value +'外側に5cmの横切開を行った。、皮下組織を切開した後。鈍的に脂肪組織を剥離し、腹直筋外膜を露出した。'
-      + '腹直筋外膜を縦切開した後、腹直筋を線維方向に分離、剥離し、腹直筋内膜を露出した。内膜を鑷子で交互につまみ上げ、腹膜下組織、臓器を落とす。内膜、腹膜を切開し、腹腔を確認した。'
-      + '切開部にたばこ縫合をかけた。\r\n'
-      + '切開部より皮下にpasserを用いてシャントチューブを、近位チューブとの接合部まで通した。'
-      const TextOfConnectionAndClosure = '遠位チューブ、圧調整バルブ、近位チューブを接合した。CSFの流出を確認した後、遠位チューブはダグラス窩に向かって30cm挿入した。'
+    }
+      const TextOfLumbarPunctureAndPocketForBulb = () => {
+        return '穿刺レベル' + punctureLevel.value + 'の傍正中にlinearに皮膚切開を行った。'
+          + '穿刺針を' + punctureLevel.value + 'に向けて'+ punctureSite.value + 'から挿入した。'
+          + '穿刺針よりCSFの流出を確認した。穿刺針より脊髄腔内に近位ドレナージチューブを'+ punctureLength.value +'cm挿入した。'
+          + '皮下にシャント圧調整バルブを挿入するスペースを作った。'
+        }
+      const TextOfVentPuncture = () => {
+        return (punctureSite.value === '前角') ? '穿刺針をnasion, 外耳孔に向けて挿入した。':'穿刺針をnasionに向けて挿入した。'
+        +'穿刺針よりCSFの流出を確認した。穿刺針を抜き、同じTractに近位ドレナージチューブを挿入した。'
+        + measurementPoint.value + 'を基準に近位ドレナージチューブを'+ punctureLength.value +'cm挿入して固定した。'
+      }
+      const TextOfAbdominalSide = () => {
+        return '臍'+ abdominalSide.value +'外側に5cmの横切開を行った。、皮下組織を切開した後。鈍的に脂肪組織を剥離し、腹直筋外膜を露出した。'
+        + '腹直筋外膜を縦切開した後、腹直筋を線維方向に分離、剥離し、腹直筋内膜を露出した。内膜を鑷子で交互につまみ上げ、腹膜下組織、臓器を落とす。内膜、腹膜を切開し、腹腔を確認した。'
+        + '切開部にたばこ縫合をかけた。\r\n'
+        + '切開部より皮下にpasserを用いてシャントチューブを、近位チューブとの接合部まで通した。'
+      }
+      const TextOfConnectionAndClosure = () => {
+        return '遠位チューブ、圧調整バルブ、近位チューブを接合した。CSFの流出を確認した後、遠位チューブはダグラス窩に向かって30cm挿入した。'
       + '腹直筋内膜、腹膜のたばこ縫合を結紮しチューブを適度な締め付けで固定した。\r\n'
       + '近位部、遠位部共に皮下を3-0バイクリルで縫合し、表皮をステープラーで縫合した。'
+      } 
 
       //V-P shunt text
       // [burrhole + pocket for bulb, puncture, abdominal side, connection, closure]
       // [burrhole + pocket for bulb, abdominal side, puncture, connection, closure]
       // L-P shunt text
       // [lumbar puncture + pocket for bulb, abdominal side, puncture, connection, closure]
-      const operationProcedureArray = (operationType.value === 'L-P shunt') ? [TextOfLumbarPunctureAndPocketForBulb, TextOfAbdominalSide, TextOfConnectionAndClosure]:
-        (punctureTiming.value === '先に') ? [TextOfBurrHoleAndPocketForBulb, TextOfVentPuncture, TextOfAbdominalSide, TextOfConnectionAndClosure]:
-        [TextOfBurrHoleAndPocketForBulb, TextOfAbdominalSide, TextOfVentPuncture, TextOfConnectionAndClosure];
+      // const operationProcedureArray = (operationType.value === 'L-P shunt') ? [TextOfLumbarPunctureAndPocketForBulb, TextOfAbdominalSide, TextOfConnectionAndClosure]:
+      //   (punctureTiming.value === '先に') ? [TextOfBurrHoleAndPocketForBulb, TextOfVentPuncture, TextOfAbdominalSide, TextOfConnectionAndClosure]:
+      //   [TextOfBurrHoleAndPocketForBulb, TextOfAbdominalSide, TextOfVentPuncture, TextOfConnectionAndClosure];
 
 
     const opeInfoItems = {
@@ -213,6 +221,10 @@
     }
 
     function createRecordForEachOperation() {
+      const operationProcedureArray = (operationType.value === 'L-P shunt') ? [TextOfLumbarPunctureAndPocketForBulb(), TextOfAbdominalSide(), TextOfConnectionAndClosure()]:
+        (punctureTiming.value === '先に') ? [TextOfBurrHoleAndPocketForBulb(), TextOfVentPuncture(), TextOfAbdominalSide(), TextOfConnectionAndClosure()]:
+        [TextOfBurrHoleAndPocketForBulb(), TextOfAbdominalSide(), TextOfVentPuncture(), TextOfConnectionAndClosure()];
+
       const operationRecordText = operationProcedureArray.join('\n');
       const opeInfoText = createOpeInfoItemsText(opeInfoItems);
       // return operationInfo + '\n\n' + operationProcedureArray +'\n\n' + opeInfoItems;

d32c926ff33cdd879be888f02032b2d0786cce75
kei <kein312@gmail.com>
2024-10-09 14:23:04 +0900

    不要なコメント行削除

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import './assets/tailwind.css';
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-// import { loadFonts } from './plugins/vuetify';
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-// loadFonts();
-
 
 const vuetify = createVuetify({
   components,
@@ -22,5 +16,4 @@ const vuetify = createVuetify({
 })
 
 const app = createApp(App)
-
 app.use(router).use(vuetify).mount('#app')
